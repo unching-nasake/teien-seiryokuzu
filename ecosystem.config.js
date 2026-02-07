c3ddde3d999337455706a9480bef39c710203094
ikuuy <ikuuy.arbdmst@gmail.com>
2026-02-07 15:59:40 +0900

    サーバー設定ファイルのメモリ使用量を変更

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
       instances: 1, // シングルプロセス（Worker Threadsで計算分散）
       exec_mode: "fork",
       watch: false,
-      max_memory_restart: "6G",
-      node_args: "--max-old-space-size=6144",
+      max_memory_restart: "3G",
+      node_args: "--max-old-space-size=3072",
       env: {
         NODE_ENV: "production",
       },
